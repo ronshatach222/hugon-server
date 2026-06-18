ae1ae74403ee54f6150d07e11f19d8f55d9ab7b8
Ron Shatach <ronshatach@h-MacBook-Air-sl-Ron.local>
2026-06-19 02:58:54 +0300

    feat: create keep alive endpoint

---
+++ client/script.js
@@ -0,0 +1,9 @@
+const content = document.getElementById("content");
+
+document.getElementById("hugBtn").onclick = () => {
+    content.innerText = "חוג";
+};
+
+document.getElementById("pacifierBtn").onclick = () => {
+    content.innerText = "מוצץ";
+};
