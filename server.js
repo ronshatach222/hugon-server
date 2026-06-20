5560ce224b9f05042c3b523b84ed479fef91f63d
Ron Shatach <ronshatach@h-MacBook-Air-sl-Ron.local>
2026-06-20 23:27:31 +0300

    feat: add mongodb players endpoints

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+app.use(express.json());
+mongoose.connect(
+    "mongodb+srv://ronshatach222_db_user:pnC1ACsfmjtrcFn0@cluster0.4ytsvqe.mongodb.net/?appName=Cluster0"
+)
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+const playerSchema = new mongoose.Schema({
+    name: String
+});
+
+const Player = mongoose.model("Player", playerSchema);
+app.post("/players", async (req, res) => {
+    const player = await Player.create({
+        name: req.body.name
+    });
+
+    res.json(player);
+});
+app.get("/players", async (req, res) => {
+    const players = await Player.find();
+
+    res.json(players);
+});
+
 
 app.get("/keep-alive", (req, res) => {
     res.json(true);
